fe5920f04b75285663ef101e48bffca497ea02c5
ognelis <ognelisar@gmail.com>
2014-10-07 20:28:25 +0700

    Add function: isSeparate

--- total_lab/main.cpp
+++ total_lab/main.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
+bool isSeparator(char& symbol);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << isSeparator(abc) << endl;
     return 0;
 }
 
+bool isSeparator(char& symbol)
+{
+    switch(symbol)
+    {
+        case ' ' :
+        case '\n':
+        case '\t':
+            return (true);
+        default:
+            return (false);
+            break;
+    }
+}
