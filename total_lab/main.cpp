47a92b203c95c9f9e4a5157cf2f54e261aae7936
ognelis <ognelisar@gmail.com>
2014-10-13 17:19:55 +0700

    added function of reading arguments from command line.

--- total_lab/main.cpp
+++ total_lab/main.cpp
@@ -12,21 +12,23 @@ struct log
     size_t nWordsInLines = 0;
 };
 
-bool isSeparator(char symbol);
-bool isString(char symbol);
+bool isSeparator(char& symbol);
+bool isString(char& symbol);
+void getCommandLineArguments(size_t num_args, char **arg_strings);
 char* getFileName();
-log getFile(const char* name);
-void readLog(log text);
+log& getFile(const char* name);
+void readLog(log* text);
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    log  out = getFile("/home/ognelis/cisco_system_programming/total_lab/god.txt");
-    readLog(&out);
+    getCommandLineArguments(argc,argv);
+    //log out = getFile("/home/ognelis/cisco_system_programming/total_lab/god.txt");
+    //readLog(&out);
     return 0;
 }
 
-bool isSeparator(char symbol)
+bool isSeparator(char& symbol)
 {
     switch(symbol)
     {
@@ -40,7 +42,7 @@ bool isSeparator(char symbol)
     }
 }
 
-bool isString(char symbol)
+bool isString(char& symbol)
 {
     if (symbol == '\n')
     {
@@ -52,27 +54,49 @@ bool isString(char symbol)
     }
 }
 
+void getCommandLineArguments(size_t num_args, char **arg_strings)
+{
+    if (num_args != 1)
+    {
+        for (size_t i = 0; i < num_args; i++)
+        {
+            log out = getFile(arg_strings[i]);
+            readLog(&out);
+        }
+    }
+    else
+    {
+        log out = getFile(getFileName());
+        readLog(&out);
+    }
+}
+
 char* getFileName()
 {
     char* name = new char [256];
     char* buffer = name;
     int symbol;
+    cout << "write file name with dir: ";
     while ((symbol = getchar()) != EOF  )
     {
         *buffer = symbol;
         buffer++;
     }
     *buffer = '\0';
+    cout << endl;
     return (name);
 }
 
 // TODO: доделать
-log getFile(const char* name)
+log& getFile(const char* name)
 {
-    log out;
+    log* out = new log;
     char symbol;
     FILE *fp;
-    fp=fopen(name,"r");
+    if ((fp=fopen(name,"r")) == NULL)
+    {
+        cout << "\nFile isn't opened: " << name <<endl;
+    }
     bool separator;
 
     while ((symbol = fgetc(fp)) != EOF)
@@ -80,31 +104,31 @@ log getFile(const char* name)
         if (isSeparator(symbol) == true)
         {
            if (isString(symbol) == true)
-               out.nLines++;
+               out->nLines++;
 
            separator = true;
         }
         else
         {
            if (separator == true)
-               out.nWordsInLines++;
+               out->nWordsInLines++;
 
            separator=false;
         }
-        out.nSymbols++;
+        out->nSymbols++;
     }
     fclose(fp);
 
-    if (out.nSymbols > 0) out.nSymbols--;
-    if (out.nLines == 0) out.nLines = 1;
-    if (separator == true) out.nWordsInLines++;
+    if (out->nSymbols > 0) out->nSymbols--;
+    if (out->nLines == 0) out->nLines = 1;
+    if (separator == true) out->nWordsInLines++;
 
-    return(out);
+    return(*out);
 }
 
-void readLog(log text)
+void readLog(log* text)
 {
-    cout <<"There is(are): " << text.nSymbols << " symbols." <<endl;
-    cout <<"There is(are): " << text.nLines   << "  lines."   <<endl;
-    cout <<"There is(are): " << text.nWordsInLines << " words." <<endl;
+    cout <<"There is(are): " << text->nSymbols << " symbols." <<endl;
+    cout <<"There is(are): " << text->nLines   << "  lines."   <<endl;
+    cout <<"There is(are): " << text->nWordsInLines << "    words.\n" <<endl;
 }
